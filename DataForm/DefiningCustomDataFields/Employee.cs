a1a10f8b295d46f0409c3c6539f8107a419a5c0b
Jingfei Hu <jingfei.hu@gmail.com>
2015-08-15 09:14:26 +0800

    Add a RadMaskedTextInput to a RadDataForm

--- DataForm/DefiningCustomDataFields/Employee.cs
+++ DataForm/DefiningCustomDataFields/Employee.cs
@@ -1,13 +1,21 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Globalization;
 using System.Linq;
 using System.Text;
 using System.Collections.ObjectModel;
+using WpfApplication1;
+using WpfApplication1.Annotations;
 
 namespace DefiningCustomDataFields
 {
-    public class Employee
+    public class Employee : INotifyPropertyChanged
     {
+        private string _ipText;
+        private IpAddressPresentation _ipValue;
+
         public string FirstName
         {
             get;
@@ -45,6 +53,52 @@ namespace DefiningCustomDataFields
             set;
         }
 
+        public string IpText
+        {
+            get { return _ipText; }
+            set
+            {
+                if (!string.IsNullOrEmpty(value) && value != this._ipText)
+                {
+                    this._ipText = value;
+                    this.ParseIpString(value);
+                    this.OnPropertyChanged("IpText");
+                }
+            }
+        }
+
+        private void ParseIpString(string ipText)
+        {
+            string[] parts = ipText.Split(new char[] { '.' });
+
+            byte partA = 0, partB = 0, partC = 0, partD = 0;
+
+            if ((byte.TryParse(parts[0], NumberStyles.Integer, CultureInfo.InvariantCulture, out partA) &&
+            byte.TryParse(parts[1], NumberStyles.Integer, CultureInfo.InvariantCulture, out partB) &&
+            byte.TryParse(parts[2], NumberStyles.Integer, CultureInfo.InvariantCulture, out partC) &&
+            byte.TryParse(parts[3], NumberStyles.Integer, CultureInfo.InvariantCulture, out partD)) == false)
+            {
+                throw new ValidationException("Incorrect format of Ip address!");
+            };
+
+            this.IpValue = new IpAddressPresentation(partA, partB, partC, partD);
+        }    
+
+        public IpAddressPresentation IpValue
+        {
+            get { return _ipValue; }
+            set 
+            {
+                if (value != this._ipValue)
+                {
+                    _ipValue = value;
+                    this.OnPropertyChanged("IpValue");
+                }
+            }
+        }
+
+        public IPAddress IpAddress { get; set; }
+
         public Employee()
         { }
 
@@ -55,7 +109,21 @@ namespace DefiningCustomDataFields
             employees.Add(new Employee() { FirstName = "Jane", LastName = "Simpson", Occupation = "Security", StartingDate = new DateTime(2008, 12, 03), IsMarried = true, Salary = 2000, Gender = Gender.Female });
             employees.Add(new Employee() { FirstName = "John", LastName = "Peterson", Occupation = "Consultant", StartingDate = new DateTime(2005, 04, 12), IsMarried = false, Salary = 2600, Gender = Gender.Male });
             employees.Add(new Employee() { FirstName = "Peter", LastName = "Bush", Occupation = "Cashier", StartingDate = new DateTime(2005, 04, 12), IsMarried = true, Salary = 2300, Gender = Gender.Male });
+
+            foreach (var employee in employees)
+            {
+                employee.IpText = "192.168.1.100";
+            }
             return employees;
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        [NotifyPropertyChangedInvocator]
+        protected virtual void OnPropertyChanged(string propertyName)
+        {
+            PropertyChangedEventHandler handler = PropertyChanged;
+            if (handler != null) handler(this, new PropertyChangedEventArgs(propertyName));
         }
     }
 
@@ -63,5 +131,21 @@ namespace DefiningCustomDataFields
     {
         Female,
         Male
+    }
+
+    public class IPAddress : INotifyPropertyChanged
+    {
+        public string IpValue { get; set; }
+
+        public string IpText { get; set; }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        [NotifyPropertyChangedInvocator]
+        protected virtual void OnPropertyChanged(string propertyName)
+        {
+            PropertyChangedEventHandler handler = PropertyChanged;
+            if (handler != null) handler(this, new PropertyChangedEventArgs(propertyName));
+        }
     }
 }
---
+++ DataForm/DefiningCustomDataFields/IpAddressField.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using Telerik.Windows.Controls;
+
+namespace WpfApplication1
+{
+    public class IpAddressField : DataFormDataField
+    {
+        protected override DependencyProperty GetControlBindingProperty()
+        {
+            return RadMaskedTextInput.ValueProperty;
+        }
+
+        protected override Control GetControl()
+        {
+            DependencyProperty dependencyProperty = this.GetControlBindingProperty();
+            var input = new RadMaskedTextInput();
+            input.Style = (Style) FindResource("IpMaskedInputStyle");
+
+            if (this.DataMemberBinding != null)
+            {
+                var binding = this.DataMemberBinding;
+                input.SetBinding(dependencyProperty, binding);
+            }
+            input.SetBinding(RadMaskedTextInput.IsEnabledProperty, new Binding("IsReadOnly") { Source = this, Converter = new InvertedBooleanConverter() });
+
+            return input;
+        }
+    }
+}
